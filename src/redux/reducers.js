a39fdfb7c5a5aa79f1b52da5587342af70294f97
AntiHexCode <131706131+AntiHexCode@users.noreply.github.com>
2024-11-28 02:18:43 +0200

    Added Actions for Mushaf Index Screen

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,31 +1,48 @@
 // redux/reducers.js
 const initialState = {
-    isPlaying: false,
-    isVisible: true,
-  };
-  
-  const actionTypes = {
-    TOGGLE_PLAY: 'TOGGLE_PLAY',
-    SHOW_NAV: 'SHOW_NAV',
-    HIDE_NAV: 'HIDE_NAV',
-  };
-  
-  export const togglePlay = () => ({ type: actionTypes.TOGGLE_PLAY });
-  export const showNav = () => ({ type: actionTypes.SHOW_NAV });
-  export const hideNav = () => ({ type: actionTypes.HIDE_NAV });
-  
-  const rootReducer = (state = initialState, action) => {
-    switch (action.type) {
-      case actionTypes.TOGGLE_PLAY:
-        return { ...state, isPlaying: !state.isPlaying };
-      case actionTypes.SHOW_NAV:
-        return { ...state, isVisible: true };
-      case actionTypes.HIDE_NAV:
-        return { ...state, isVisible: false };
-      default:
-        return state;
-    }
-  };
-  
-  export default rootReducer;
-  
+  isPlaying: false,
+  isVisible: true,
+    
+  // for Mushaf Index Screen
+  mushafIndexType: 'Chapter'
+};
+
+const actionTypes = {
+  TOGGLE_PLAY: 'TOGGLE_PLAY',
+  SHOW_NAV: 'SHOW_NAV',
+  HIDE_NAV: 'HIDE_NAV',
+    
+  // for Mushaf Index Screen
+  ACTIVATE_JUZ_INDEX_TYPE: 'CHANGE_INDEX_TYPE_TO_JUZ',
+  ACTIVATE_CHAPTER_INDEX_TYPE: 'CHANGE_INDEX_TYPE_TO_CHAPTER'
+};
+
+export const togglePlay = () => ({ type: actionTypes.TOGGLE_PLAY });
+export const showNav = () => ({ type: actionTypes.SHOW_NAV });
+export const hideNav = () => ({ type: actionTypes.HIDE_NAV });
+
+// for Mushaf Index Screen
+export const activateJuzIndexType = () => ({ type: actionTypes.ACTIVATE_JUZ_INDEX_TYPE });
+export const activateChapterIndexType = () => ({ type: actionTypes.ACTIVATE_CHAPTER_INDEX_TYPE });
+
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.TOGGLE_PLAY:
+      return { ...state, isPlaying: !state.isPlaying };
+    case actionTypes.SHOW_NAV:
+      return { ...state, isVisible: true };
+    case actionTypes.HIDE_NAV:
+      return { ...state, isVisible: false };
+          
+    // for Mushaf Index Screen
+    case actionTypes.ACTIVATE_JUZ_INDEX_TYPE:
+      return { ...state, mushafIndexType: 'Juz'};
+    case actionTypes.ACTIVATE_CHAPTER_INDEX_TYPE:
+      return { ...state, mushafIndexType: 'Chapter'};
+
+    default:
+      return state;
+  }
+};
+
+export default rootReducer;
