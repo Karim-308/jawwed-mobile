2ebd318fe8ea4e1e6ce95a170aa7f9c5b6bc844a
Karim-308 <2001118@eng.asu.edu.eg>
2024-12-10 16:30:40 +0200

    Updated legacy Parser and Moshaf Page

--- src/utils/QuranPageJsonParser.js
+++ src/utils/QuranPageJsonParser.js
@@ -1,33 +1,27 @@
 import message from '../api/mushaf/sajda.json';
 
 const QuranPageJsonParser = () => {
-    // Function to remove the '۝' symbol from a line
     const removeAndReplaceTajweedSymbols = (text) => {
-        // Replace all instances of ۟ with the sukūn character ْ
-        text = text.replace(/۟/g, 'ْ');  // SUKUN
-        text = text.replace(/ࣰ/g, 'ٗ'); //FATHA
-        text = text.replace(/ࣱ/g, 'ٞ'); //DAMMA
-        text = text.replace(/ࣲ/g, 'ٖ'); //KASRA
-        // Removes all instances of ۝
-        return text.replace(/۝/g, '');
+        text = text.replace(/۟/g, 'ْ'); 
+        text = text.replace(/ࣰ/g, 'ٗ');
+        text = text.replace(/ࣱ/g, 'ٞ');
+        text = text.replace(/ࣲ/g, 'ٖ');
+        text = text.replace(/۝/g,'');
+        return text;
     };
 
-    const getLineText = (line) => {
-        return message[line]['text']; // Fetches the text from the JSON
-    };
-
-    // This function extracts lines and removes the symbol
     const extractLinesTextFromPage = (message) => {
-
         return message.map(line => {
-            if (line.lineNumber === 15) {
-                return removeAndReplaceTajweedSymbols(line.text); // Fetch the text and add a newline
-            }
-            return removeAndReplaceTajweedSymbols(line.text) + "\n"; // Remove '۝' and join with a newline
+            return {
+                lineID: line.lineID,
+                text: removeAndReplaceTajweedSymbols(line.text),
+                verseKeys: line.versesKeys ? line.versesKeys.split(',') : [], 
+                isCentered: line.isCentered
+            };
         });
     };
 
-    return extractLinesTextFromPage(message); // Return the Ayah content for the given line
+    return extractLinesTextFromPage(message);
 };
 
 export default QuranPageJsonParser;
