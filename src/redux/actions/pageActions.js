3636dcd9f9cabe1a4b95de6d8b8b0954c4c60f0c
Karim-308 <2001118@eng.asu.edu.eg>
2024-12-25 20:53:36 +0200

    Refactor pageReducer and pageActions to improve logging and clarify action creators; remove unnecessary comments for better code readability

--- src/redux/actions/pageActions.js
+++ src/redux/actions/pageActions.js
@@ -7,10 +7,16 @@ export const FETCH_PAGE_FAILURE = 'FETCH_PAGE_FAILURE';
 export const SET_PAGE_NUMBER = 'SET_PAGE_NUMBER';
 
 // Action Creators
+
+// Action Creator for Fetching Page Data Request
+// This action creator is used to dispatch the loading state when fetching page data is requested 
 export const fetchPageRequest = () => ({
   type: FETCH_PAGE_REQUEST,
 });
 
+// Action Creator for Fetching Page Data Success
+// This action creator is used to dispatch the page data when fetching page data is successful
+// This action creator is used to dispatch the page data when fetching page data is successful
 export const fetchPageSuccess = (pageNumber, data) => {
   console.log('FETCH_PAGE_SUCCESS triggered for page:', pageNumber);
   console.log('Data:', data);  // Log the raw data (array of lines)
@@ -24,18 +30,23 @@ export const fetchPageSuccess = (pageNumber, data) => {
   };
 };
 
-
+// Action Creator for Fetching Page Data Failure
+// This action creator is used to dispatch the error message when fetching page data fails
 export const fetchPageFailure = (error) => ({
   type: FETCH_PAGE_FAILURE,
   payload: error,
 });
-
+ 
+// Action Creator for Setting the Page Number
+// This action creator is used to set the current page number in the state
+// This action is dispatched when the user navigates to a new page
 export const setPageNumber = (pageNumber) => ({
   type: SET_PAGE_NUMBER,
   payload: pageNumber,
 });
 
 // Thunk Action Creator for Fetching Page Data
+// This action creator fetches the page data from the API and dispatches the appropriate actions
 export const fetchPageData = (pageNumber) => async (dispatch) => {
   dispatch(fetchPageRequest());
   try {
