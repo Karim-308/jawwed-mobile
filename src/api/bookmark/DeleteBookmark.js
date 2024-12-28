c5f8bd64896d17626c0ee4a607057c02bdcb9d1d
Karim-308 <2001118@eng.asu.edu.eg>
2024-12-28 14:45:21 +0200

    Add success and error feedback for bookmark deletion and posting

--- src/api/bookmark/DeleteBookmark.js
+++ src/api/bookmark/DeleteBookmark.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-
+import { ToastAndroid, Alert, Platform } from 'react-native';
 const deleteBookmark = async (userId, verseKey) => {
   const url = 'http://jawwed-api.runasp.net/api/Bookmark';
 
@@ -10,7 +10,15 @@ const deleteBookmark = async (userId, verseKey) => {
     });
 
     // Handle success
-    console.log(`Bookmark deleted successfully for userId: ${userId} and verseKey: ${verseKey}`);
+    console.log('Bookmark successfully posted:', response.data);
+    // Display success message
+    if (Platform.OS === 'android') {
+      ToastAndroid.show('Bookmark Deleted', ToastAndroid.SHORT);
+    } else {
+      Alert.alert('Success', 'Bookmark Deleted');
+    }
+    
+
     return response.data;
   } catch (error) {
     // Handle errors
@@ -24,6 +32,15 @@ const deleteBookmark = async (userId, verseKey) => {
       // Something else happened while setting up the request
       console.error('Error message:', error.message);
     }
+
+    // Show error feedback to the user
+         // I did "Bookmark already saved" but it can be anything this is just for demo
+         if (Platform.OS === 'android') {
+          ToastAndroid.show('Failed to delete bookmark', ToastAndroid.SHORT);
+        } else {
+          Alert.alert('Error', 'Failed to delete bookmark');
+        }
+
     throw error; // Re-throw the error for further handling
   }
 };
