a80508f64a57f25ba400e09ecb087535b782aa06
AntiHexCode <131706131+AntiHexCode@users.noreply.github.com>
2024-12-28 04:59:54 +0200

    Added isPaused state

--- src/redux/reducers/audioReducer.js
+++ src/redux/reducers/audioReducer.js
@@ -1,18 +1,21 @@
 // redux/reducers/audioReducer.js
 const initialState = {
     isPlaying: false,
+    isPaused: false,
     audioRepeat: false,
     audioPanelVisible: false,
   };
   
   const actionTypes = {
     TOGGLE_PLAY: 'TOGGLE_PLAY',
+    TOGGLE_PAUSE: 'TOGGLE_PAUSE',
     TOGGLE_AUDIO_REPEAT: 'TOGGLE_AUDIO_REPEAT',
     TOGGLE_AUDIO_PANEL: 'TOGGLE_AUDIO_PANEL',
   };
   
   // Action Creators
   export const togglePlay = () => ({ type: actionTypes.TOGGLE_PLAY });
+  export const togglePause = () => ({ type: actionTypes.TOGGLE_PAUSE });
   export const toggleAudioRepeat = () => ({ type: actionTypes.TOGGLE_AUDIO_REPEAT });
   export const toggleAudioPanel = () => ({ type: actionTypes.TOGGLE_AUDIO_PANEL });
   
@@ -20,6 +23,8 @@ const initialState = {
     switch (action.type) {
       case actionTypes.TOGGLE_PLAY:
         return { ...state, isPlaying: !state.isPlaying };
+      case actionTypes.TOGGLE_PAUSE:
+        return { ...state, isPaused: !state.isPaused };
       case actionTypes.TOGGLE_AUDIO_REPEAT:
         return { ...state, audioRepeat: !state.audioRepeat };
       case actionTypes.TOGGLE_AUDIO_PANEL:
