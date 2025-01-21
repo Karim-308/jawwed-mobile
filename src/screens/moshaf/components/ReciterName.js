914c1f727789b90fa5dc74e4ec6b4955ecd5b1e0
AntiHexCode <131706131+AntiHexCode@users.noreply.github.com>
2025-01-21 17:47:57 +0200

    Enhanced Reciters selection Modal UI

---
+++ src/screens/moshaf/components/ReciterName.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+import { PRIMARY_GOLD } from '../../../constants/colors';
+
+export default function ReciterName({name}) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.reciterNameText}>الشيخ/ {name}</Text>
+      <MaterialIcons name="volume-up" style={styles.icon} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection:'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderStyle: 'solid',
+    borderColor: PRIMARY_GOLD,
+    borderWidth: 1,
+    borderRadius: 10,
+    height: 45
+  },
+  reciterNameText: {
+    width: 120,
+    textAlign: 'right',
+    color: '#fff',
+    fontSize: 16,
+    marginRight: 15
+  },
+  icon: {
+    fontSize: 24,
+    color: `${PRIMARY_GOLD}`,
+    transform: [{rotate: '180deg'}],
+    marginRight: 20
+  }
+});
