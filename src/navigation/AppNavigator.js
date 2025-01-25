96183883105f727a8a73953087f996c3cdc5afbe
Karim-308 <2001118@eng.asu.edu.eg>
2025-01-25 17:16:35 +0200

    Splash Background added Refactor app navigation structure and update assets for improved navigation and branding

---
+++ src/navigation/AppNavigator.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'expo-status-bar';
+import HomeScreen from '../screens/home/HomeScreen';
+import MoshafIndexScreen from '../screens/moshaf-index/MoshafIndexScreen';
+import MoshafScreen from '../screens/moshaf/MoshafScreen';
+import BookmarkScreen from '../screens/bookmark/BookmarkScreen';
+import Header from '../screens/moshaf/components/MoshafHeader';
+import { PRIMARY_GOLD } from '../constants/colors';
+
+const Stack = createStackNavigator();
+
+const AppNavigator = () => (
+  <NavigationContainer>
+    <StatusBar style="light" />
+    <Stack.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerStyle: { backgroundColor: '#000', elevation: 0, shadowOpacity: 0 },
+        headerTintColor: '#FFF',
+        headerTitleStyle: { fontWeight: 'bold' },
+        headerBackTitleVisible: false,
+      }}
+    >
+      <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+      <Stack.Screen
+        name="IndexPage"
+        component={MoshafIndexScreen}
+        options={{
+          title: 'الفهرس',
+          headerTitleStyle: {
+            fontFamily: 'UthmanicHafs',
+            fontSize: 30,
+          },
+          headerTitleAlign: 'center',
+        }}
+      />
+      <Stack.Screen
+        name="BookmarkPage"
+        component={BookmarkScreen}
+        options={{
+          title: 'الإشارات المرجعية',
+          headerTitleStyle: {
+            fontFamily: 'UthmanicHafs',
+            fontSize: 30,
+          },
+          headerTitleAlign: 'center',
+        }}
+      />
+      <Stack.Screen
+        name="MoshafPage"
+        component={MoshafScreen}
+        options={{
+          headerTitle: () => <Header />,
+          headerTintColor: `${PRIMARY_GOLD}`,
+          headerTitleStyle: {
+            fontFamily: 'UthmanicHafs',
+            fontSize: 30,
+          },
+          headerTitleAlign: 'center',
+          headerStyle: {
+            backgroundColor: '#000',
+          },
+        }}
+      />
+    </Stack.Navigator>
+  </NavigationContainer>
+);
+
+export default AppNavigator;
