59be63d0abcdb934f27e6e79bd5fd8877548fabe
mohamed-khaled-xv <mohamedkhaledxv8@gmail.com>
2025-06-20 15:07:24 +0300

    the entire home screen is scrollable, also hijri date has been added

--- src/screens/home/HomeScreen.js
+++ src/screens/home/HomeScreen.js
@@ -20,12 +20,14 @@ import { useNavigation, useFocusEffect } from "@react-navigation/native";
 import { PrayerTimes, CalculationMethod, Coordinates, Madhab } from "adhan";
 import { get } from "../../utils/localStorage/secureStore";
 import Colors from "../../constants/newColors";
-import { useSelector } from 'react-redux';
-import { indexTypesItems } from '../moshaf-index/components/IndexData';
-import BookIcon from '../../assets/images/open-book.png'; // adjust path if needed
-import Basmallah from '../../assets/images/basmallah.png'; // adjust path if needed
-import LastReadIcon from '../../assets/images/last-read.png'; // adjust path if needed
-import LastReadCard from './components/LastReadCard';
+import { useSelector } from "react-redux";
+import { indexTypesItems } from "../moshaf-index/components/IndexData";
+import BookIcon from "../../assets/images/open-book.png"; // adjust path if needed
+import Basmallah from "../../assets/images/basmallah.png"; // adjust path if needed
+import LastReadIcon from "../../assets/images/last-read.png"; // adjust path if needed
+import LastReadCard from "./components/LastReadCard";
+import moment from "moment-hijri";
+import "moment/locale/ar"; // for Arabic support
 
 const prayerNamesArabic = {
   fajr: "الفجر",
@@ -34,6 +36,21 @@ const prayerNamesArabic = {
   maghrib: "المغرب",
   isha: "العشاء",
 };
+
+const hijriMonthsArabic = [
+  "محرم",
+  "صفر",
+  "ربيع الأول",
+  "ربيع الآخر",
+  "جمادى الأولى",
+  "جمادى الآخرة",
+  "رجب",
+  "شعبان",
+  "رمضان",
+  "شوال",
+  "ذو القعدة",
+  "ذو الحجة",
+];
 
 const toArabicNumber = (num) => {
   return num
@@ -49,69 +66,70 @@ const HomeScreen = () => {
   const [remainingSeconds, setRemainingSeconds] = useState(0);
   const [darkMode, setDarkMode] = useState(true);
   const [searchQuery, setSearchQuery] = useState("");
+  const [hijriDate, setHijriDate] = useState("");
 
   // Define features data
   const features = [
     {
-      id: 'index',
-      name: 'الفهرس',
-      icon: 'menu-book',
-      iconType: 'MaterialIcons',
-      screen: 'IndexPage'
+      id: "index",
+      name: "الفهرس",
+      icon: "menu-book",
+      iconType: "MaterialIcons",
+      screen: "IndexPage",
     },
     {
-      id: 'moshaf',
-      name: 'المصحف',
-      icon: 'import-contacts',
-      iconType: 'MaterialIcons',
-      screen: 'MoshafPage'
+      id: "moshaf",
+      name: "المصحف",
+      icon: "import-contacts",
+      iconType: "MaterialIcons",
+      screen: "MoshafPage",
     },
     {
-      id: 'bookmark',
-      name: 'الإشارات المرجعية',
-      icon: 'bookmarks-outline',
-      iconType: 'Ionicons',
-      screen: 'BookmarkPage'
+      id: "bookmark",
+      name: "الإشارات المرجعية",
+      icon: "bookmarks-outline",
+      iconType: "Ionicons",
+      screen: "BookmarkPage",
     },
     {
-      id: 'azkar',
-      name: 'الأذكار',
-      icon: 'AzkarIcon',
-      iconType: 'Image-Azkar',
-      screen: 'AzkarPage'
+      id: "azkar",
+      name: "الأذكار",
+      icon: "AzkarIcon",
+      iconType: "Image-Azkar",
+      screen: "AzkarPage",
     },
     {
-      id: 'prayer',
-      name: 'مواقيت الصلاة',
-      icon: 'time-outline',
-      iconType: 'Ionicons',
-      screen: 'PrayerTimesPage'
+      id: "prayer",
+      name: "مواقيت الصلاة",
+      icon: "time-outline",
+      iconType: "Ionicons",
+      screen: "PrayerTimesPage",
     },
     {
-      id: 'quiz',
-      name: 'اختبار القرآن',
-      icon: 'chalkboard-teacher',
-      iconType: 'FontAwesome5',
-      screen: 'QuizPage'
+      id: "quiz",
+      name: "اختبار القرآن",
+      icon: "chalkboard-teacher",
+      iconType: "FontAwesome5",
+      screen: "QuizPage",
     },
     {
-      id: 'qiblah',
-      name: 'اتجاه القِبلة',
-      icon: 'compass-outline',
-      iconType: 'MaterialCommunityIcons',
-      screen: 'QiblahPage'
+      id: "qiblah",
+      name: "اتجاه القِبلة",
+      icon: "compass-outline",
+      iconType: "MaterialCommunityIcons",
+      screen: "QiblahPage",
     },
     {
-      id: 'masbaha',
-      name: 'سبحة',
-      icon: 'AzkarIcon',
-      iconType: 'Image-Sebha',
-      screen: 'MasbahaPage'
-    }
+      id: "masbaha",
+      name: "سبحة",
+      icon: "AzkarIcon",
+      iconType: "Image-Sebha",
+      screen: "MasbahaPage",
+    },
   ];
 
   // Filter features based on search query
-  const filteredFeatures = features.filter(feature =>
+  const filteredFeatures = features.filter((feature) =>
     feature.name.includes(searchQuery)
   );
 
@@ -188,38 +206,88 @@ const HomeScreen = () => {
     };
   }, []);
 
+  useEffect(() => {
+    const now = moment();
+    const hijriDay = now.iDate(); // day of month
+    const hijriMonth = hijriMonthsArabic[now.iMonth()]; // 0-based index
+    const hijriYear = now.iYear();
+
+    const formattedHijriDate = `${toArabicNumber(
+      hijriDay
+    )} ${hijriMonth} ${toArabicNumber(hijriYear)}`;
+    setHijriDate(formattedHijriDate);
+  }, []);
+
   const formatTime = (seconds) => {
     const hrs = Math.floor(seconds / 3600);
     const mins = Math.floor((seconds % 3600) / 60);
     const secs = seconds % 60;
-    return `${toArabicNumber(hrs)}:${toArabicNumber(mins)}:${toArabicNumber(secs)}`;
+    return `${toArabicNumber(hrs)}:${toArabicNumber(mins)}:${toArabicNumber(
+      secs
+    )}`;
   };
 
   const currentColors = darkMode ? Colors.dark : Colors.light;
 
   const renderIcon = (feature) => {
     switch (feature.iconType) {
-      case 'MaterialIcons':
-        return <MaterialIcons name={feature.icon} size={40} color={Colors.highlight} style={styles.iconShine} />;
-      case 'Ionicons':
-        return <Ionicons name={feature.icon} size={40} color={Colors.highlight} style={styles.iconShine} />;
-      case 'FontAwesome5':
-        return <FontAwesome5 name={feature.icon} size={40} color={Colors.highlight} style={styles.iconShine} />;
-      case 'MaterialCommunityIcons':
-        return <MaterialCommunityIcons name={feature.icon} size={40} color={Colors.highlight} style={styles.iconShine} />;
-      case 'Image-Azkar':
+      case "MaterialIcons":
+        return (
+          <MaterialIcons
+            name={feature.icon}
+            size={40}
+            color={Colors.highlight}
+            style={styles.iconShine}
+          />
+        );
+      case "Ionicons":
+        return (
+          <Ionicons
+            name={feature.icon}
+            size={40}
+            color={Colors.highlight}
+            style={styles.iconShine}
+          />
+        );
+      case "FontAwesome5":
+        return (
+          <FontAwesome5
+            name={feature.icon}
+            size={40}
+            color={Colors.highlight}
+            style={styles.iconShine}
+          />
+        );
+      case "MaterialCommunityIcons":
+        return (
+          <MaterialCommunityIcons
+            name={feature.icon}
+            size={40}
+            color={Colors.highlight}
+            style={styles.iconShine}
+          />
+        );
+      case "Image-Azkar":
         return (
           <Image
             source={require("../../assets/images/AzkarIcon.png")}
-            style={[styles.azkarImage, { tintColor: Colors.highlight }, styles.iconShine]}
+            style={[
+              styles.azkarImage,
+              { tintColor: Colors.highlight },
+              styles.iconShine,
+            ]}
             resizeMode="contain"
           />
         );
-      case 'Image-Sebha':
+      case "Image-Sebha":
         return (
           <Image
             source={require("../../assets/images/prayer-breads.png")}
-            style={[styles.sebhaImage, { tintColor: Colors.highlight }, styles.iconShine]}
+            style={[
+              styles.sebhaImage,
+              { tintColor: Colors.highlight },
+              styles.iconShine,
+            ]}
             resizeMode="contain"
           />
         );
@@ -229,98 +297,133 @@ const HomeScreen = () => {
   };
 
   return (
-    <SafeAreaView style={[styles.container, { backgroundColor: currentColors.background, paddingTop: 24 }]}>
-      <ImageBackground
-        source={require("../../assets/images/home_background.png")}
-        style={styles.headerBackground}
-        resizeMode="cover"
+    <SafeAreaView
+      style={[
+        styles.container,
+        { backgroundColor: currentColors.background, paddingTop: 24 },
+      ]}
+    >
+      <ScrollView
+        style={styles.featuresScrollView}
+        contentContainerStyle={styles.featuresList}
+        showsVerticalScrollIndicator={false}
+        bounces={true}
+        overScrollMode="never"
+        decelerationRate="fast"
       >
-        <TouchableOpacity
-          style={styles.profileIcon}
-          onPress={() => navigation.navigate("ProfilePage")}
+        <ImageBackground
+          source={require("../../assets/images/home_background.png")}
+          style={styles.headerBackground}
+          resizeMode="cover"
         >
-          <Image
-            source={require("../../assets/images/profileiconcopy.png")}
-            style={styles.profileImage}
-            resizeMode="contain"
-          />
-        </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.profileIcon}
+            onPress={() => navigation.navigate("ProfilePage")}
+          >
+            <Image
+              source={require("../../assets/images/profileiconcopy.png")}
+              style={styles.profileImage}
+              resizeMode="contain"
+            />
+          </TouchableOpacity>
 
-        <View style={styles.header}>
-          <Text style={[styles.clock, { color: currentColors.clock }]}>
-            {currentTime.replace(/[صم]$/, "")}
-            <Text style={styles.amPm}>
-              {currentTime.slice(-1)} {/* This will be either "ص" or "م" */}
+          <View style={styles.header}>
+            <Text style={[styles.clock, { color: currentColors.clock }]}>
+              {currentTime.replace(/[صم]$/, "")}
+              <Text style={styles.amPm}>
+                {currentTime.slice(-1)} {/* This will be either "ص" or "م" */}
+              </Text>
             </Text>
-          </Text>
 
-          <Text
-            style={[styles.prayerTime, { color: currentColors.prayerTime }]}
-          >
-            {nextPrayer
-              ? `متبقي ${formatTime(remainingSeconds)} لصلاة ${nextPrayer}`
-              : "لا يوجد صلاة قادمة"}
-          </Text>
+            <Text
+              style={[styles.prayerTime, { color: currentColors.prayerTime }]}
+            >
+              {nextPrayer
+                ? `متبقي ${formatTime(remainingSeconds)} لصلاة ${nextPrayer}`
+                : "لا يوجد صلاة قادمة"}
+            </Text>
 
-          <View
-            style={[
-              styles.searchContainer,
-              { backgroundColor: currentColors.searchBackground },
-            ]}
-          >
-            <MaterialIcons name="language" size={24} color={Colors.highlight} />
-            <MaterialIcons
-              name="notifications"
-              size={24}
-              color={Colors.highlight}
-            />
-            <TextInput
-              placeholder="انقر هنـــا للبحث"
-              placeholderTextColor={currentColors.inputPlaceholder}
-              writingDirection="rtl"
-              style={[styles.searchInput, { color: currentColors.text }]}
-              value={searchQuery}
-              onChangeText={setSearchQuery}
-            />
-            {searchQuery ? (
-              <TouchableOpacity onPress={() => setSearchQuery("")}>
-                <MaterialIcons name="clear" size={24} color={Colors.highlight} />
-              </TouchableOpacity>
-            ) : (
-              <MaterialIcons name="search" size={24} color={Colors.highlight} />
-            )}
+            <Text
+              style={[styles.hijriDate, { color: currentColors.prayerTime }]}
+            >
+              {hijriDate}
+            </Text>
+
+            <View
+              style={[
+                styles.searchContainer,
+                { backgroundColor: currentColors.searchBackground },
+              ]}
+            >
+              <MaterialIcons
+                name="language"
+                size={24}
+                color={Colors.highlight}
+              />
+              <MaterialIcons
+                name="notifications"
+                size={24}
+                color={Colors.highlight}
+              />
+              <TextInput
+                placeholder="انقر هنـــا للبحث"
+                placeholderTextColor={currentColors.inputPlaceholder}
+                writingDirection="rtl"
+                style={[styles.searchInput, { color: currentColors.text }]}
+                value={searchQuery}
+                onChangeText={setSearchQuery}
+              />
+              {searchQuery ? (
+                <TouchableOpacity onPress={() => setSearchQuery("")}>
+                  <MaterialIcons
+                    name="clear"
+                    size={24}
+                    color={Colors.highlight}
+                  />
+                </TouchableOpacity>
+              ) : (
+                <MaterialIcons
+                  name="search"
+                  size={24}
+                  color={Colors.highlight}
+                />
+              )}
+            </View>
           </View>
-        </View>
-      </ImageBackground>
+        </ImageBackground>
 
-      <View style={styles.contentContainer}>
-        <LastReadCard />
-        <Text style={[styles.sectionTitle, { color: currentColors.sectionTitle }]}>القــائــــــمة</Text>
+        <View style={styles.contentContainer}>
+          <LastReadCard />
+          <Text
+            style={[styles.sectionTitle, { color: currentColors.sectionTitle }]}
+          >
+            القــائــــــمة
+          </Text>
+          <View style={styles.featuresContainer}>
 
-        <ScrollView 
-          style={styles.featuresScrollView}
-          contentContainerStyle={styles.featuresList}
-          showsVerticalScrollIndicator={false}
-          bounces={true}
-          overScrollMode="never"
-          decelerationRate="fast"
-        >
           {filteredFeatures.map((feature) => (
             <TouchableOpacity
               key={feature.id}
-              style={[styles.featureItem, { backgroundColor: currentColors.featureBackground }]}
+              style={[
+                styles.featureItem,
+                { backgroundColor: currentColors.featureBackground },
+              ]}
               onPress={() => navigation.navigate(feature.screen)}
             >
-              <View style={styles.iconContainer}>
-                {renderIcon(feature)}
-              </View>
-              <Text style={[styles.featureText, { color: currentColors.featureText }]}>
+              <View style={styles.iconContainer}>{renderIcon(feature)}</View>
+              <Text
+                style={[
+                  styles.featureText,
+                  { color: currentColors.featureText },
+                ]}
+              >
                 {feature.name}
               </Text>
             </TouchableOpacity>
           ))}
-        </ScrollView>
-      </View>
+          </View>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
@@ -386,24 +489,24 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   featuresList: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'center',
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
     paddingHorizontal: 5,
     gap: 15,
     paddingBottom: 20,
   },
   featureItem: {
-    width: '22%',
+    width: "22%",
     minWidth: 100,
     aspectRatio: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     borderRadius: 12,
     padding: 10,
-    backgroundColor: '#212020',
+    backgroundColor: "#212020",
     elevation: 3,
-    shadowColor: '#000',
+    shadowColor: "#000",
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
@@ -411,12 +514,12 @@ const styles = StyleSheet.create({
   iconContainer: {
     width: 60,
     height: 60,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     marginBottom: 8,
   },
   iconShine: {
-    shadowColor: '#fff',
+    shadowColor: "#fff",
     shadowOffset: { width: 0, height: 0 },
     shadowOpacity: 0.5,
     shadowRadius: 10,
@@ -424,8 +527,8 @@ const styles = StyleSheet.create({
   },
   featureText: {
     fontSize: 12,
-    fontWeight: 'medium',
-    textAlign: 'center',
+    fontWeight: "medium",
+    textAlign: "center",
     marginTop: 5,
   },
   azkarImage: {
@@ -452,9 +555,22 @@ const styles = StyleSheet.create({
     borderRadius: 40,
   },
   amPm: {
-    fontSize: 24,  
-    lineHeight: 36, 
+    fontSize: 24,
+    lineHeight: 36,
     marginLeft: 4,
     fontWeight: "normal",
+  },
+  hijriDate: {
+    fontSize: 18,
+    marginTop: 4,
+    textAlign: "center",
+  },
+  featuresContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    flexWrap: "wrap",
+    paddingHorizontal: 5,
+    gap: 10,
   },
 });
