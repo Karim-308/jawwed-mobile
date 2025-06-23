b968a1e295c0f6b51af20e9e8e4c8ec7e5c4807e
AntiHexCode <>
2025-06-23 16:53:59 +0200

    Added the notification request for creating new khtma

--- src/screens/khtma/components/khtma-header/KhtmaHeader.js
+++ src/screens/khtma/components/khtma-header/KhtmaHeader.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet , View , TouchableOpacity } from 'react-native';
-import { useDispatch } from 'react-redux';
-import { setIsNewGoalMenuVisible } from '../../../../redux/reducers/khtmaReducer';
+import { useSelector, useDispatch } from 'react-redux';
+import { setIsNewGoalMenuVisible, setErrorStatus } from '../../../../redux/reducers/khtmaReducer';
+import { setNotificationsEnabled } from '../../../../redux/reducers/notificationReducer';
 import { AntDesign, MaterialIcons } from '@expo/vector-icons';
 import { PRIMARY_GOLD } from '../../../../constants/colors';
 import { get } from '../../../../utils/localStorage/secureStore';
@@ -10,11 +11,21 @@ import Colors from '../../../../constants/newColors';
 
 export default function KhtmaHeader() {
 
+    // real-time notifications
+    const notificationsEnabled = useSelector((state) => state.notification.notificationsEnabled);
+    const assignNotificationsEnabled = (notificationsEnabled) => {
+        dispatch(setNotificationsEnabled(notificationsEnabled));
+    }
+
     const dispatch = useDispatch();
     
     // New Goal Menu
     const assignIsNewGoalMenuVisible = (newGoalMenuVisible) => {
         dispatch(setIsNewGoalMenuVisible(newGoalMenuVisible));
+    }
+
+    const assignErrorStatus = (errorStatus) => {
+        dispatch(setErrorStatus(errorStatus));
     }
 
     // set the dark or light mode
@@ -38,7 +49,12 @@ export default function KhtmaHeader() {
 
             {/*New Khtma Button*/}
             <TouchableOpacity
-                onPress={() => assignIsNewGoalMenuVisible(true)}
+                onPress={() => {
+                    if(notificationsEnabled)
+                        assignIsNewGoalMenuVisible(true);
+                    else
+                        assignErrorStatus('يجب عليك تفعيل الإشعارات من الملف الشخصي أولاً');
+                }}
             >
                 <AntDesign name='pluscircleo' size={24} color={currentColors.text} />
             </TouchableOpacity>
